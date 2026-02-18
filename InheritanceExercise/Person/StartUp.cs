4aac5e9c0eb57a3966f0730bd3fee63744d73ac9
Александър Давидов <ackodavidoff@gmail.com>
2026-02-18 11:15:32 +0200

    Inheritance

---
+++ InheritanceExercise/Person/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 0)
+            {
+                Console.WriteLine("Age must be positive!");
+            }
+            else if (age > 15)
+            {
+                Person person = new Person(name, age);
+                Console.WriteLine(person.ToString());
+            }
+            else
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child.ToString());
+            }
+
+        }
+    }
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"Person -> Name: {this.Name}, Age: {this.Age}";//base.ToString();
+        }
+    }
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+        public override string ToString()
+        {
+            return $"Child -> Name: {this.Name}, Age: {this.Age}";//base.ToString();
+        }
+    }
+}
---
+++ InheritanceExercise/Zoo/StartUp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+    public class Animal
+    {
+        public Animal(string  name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+    public class Reptile : Animal
+    {
+        public Reptile(string name) : base(name)
+        {
+        }
+    }
+    public class Lizard : Reptile
+    {
+        public Lizard(string name) : base(name)
+        {
+        }
+    }
+    public class Snake : Reptile
+    {
+        public Snake(string name) : base(name)
+        {
+        }
+    }
+    public class Mammal : Animal
+    {
+        public Mammal(string name) : base(name)
+        {
+        }
+    }
+    //public class Reptile : Animal
+    //{
+    //    public Reptile(string name) : base(name)
+    //    {
+    //    }
+    //}
+    public class Gorilla : Mammal
+    {
+        public Gorilla(string name) : base(name)
+        {
+        }
+    }
+    public class Bear : Mammal
+    {
+        public Bear(string name) : base(name)
+        {
+        }
+    }
+}
